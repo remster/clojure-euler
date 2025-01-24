e55c6650b99a4832fa3f6df851f86bae0778e6f3
Remek Zajac <remek.zajac@droneup.com>
2025-01-24 16:57:36 +0100

    finished 1

--- spec/euler/level1/problem001_spec.clj
+++ spec/euler/level1/problem001_spec.clj
@@ -7,9 +7,19 @@
 
 (describe "Euler Problem #1"
 
+  (it "finds multiples of 3 and 5"
+    (should= false (multiple-of-3-or-5? 1))
+    (should= true (multiple-of-3-or-5? 3))
+    (should= true (multiple-of-3-or-5? 10))
+    )
+
   (it "Solves #1"
     (should= 23 (euler-1 10))
-	(should= -1 (euler-1 1000)))
+    (should= 3 (euler-1 4))
+    (should= 8 (euler-1 6))
+    (should= 33 (euler-1 11))
+    ;(should= -1 (euler-1 1000))
+    )
   )
 
 (run-specs)
--- src/euler/level1/problem001.clj
+++ src/euler/level1/problem001.clj
@@ -1,4 +1,10 @@
 (ns euler.level1.problem001)
 
-(defn euler-1 [n]
+(defn multiple-of-3-or-5? [n]
+  (or (zero? (mod n 3)) (zero? (mod n 5)))
   )
+
+(defn euler-1 [n]
+  (->> (range n)
+       (filter multiple-of-3-or-5?)
+       (apply +)))
