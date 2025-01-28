1a931b980e0c6c7b6aa5007a6f7d02fb0619476f
Remek Zajac <remek.zajac@droneup.com>
2025-01-28 13:26:04 +0100

    8

---
+++ spec/euler/level1/problem008_spec.clj
@@ -0,0 +1,20 @@
+(ns euler.level1.problem008-spec
+  (:require
+    [speclj.core :refer :all]
+    [euler.level1.problem008 :refer :all]))
+
+; https://projecteuler.net/problem=8
+
+(describe "Euler Problem #8"
+
+  (it "Solves #8"
+    (should= "7316" (first (segment-by-digits 4)))
+    (should= "3167" (second (segment-by-digits 4)))
+    (should= "3450" (last (segment-by-digits 4)))
+    (should= (* 2 3 4) (digits-product 234))
+    (should= 5832 (euler-8 4))
+    (should= 23514624000 (euler-8 13))
+    )
+  )
+
+(run-specs)
