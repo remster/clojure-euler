50a86cc64a816aab0e4911fa7cdeba495476486b
Remek Zajac <remek.zajac@droneup.com>
2025-01-31 08:26:33 +0100

    rolls

--- spec/euler/level1/bowling_spec.clj
+++ spec/euler/level1/bowling_spec.clj
@@ -7,10 +7,23 @@
 
 (describe "Bowling Game"
 
-  (it "Calculates Score"
+  (it "The score function must iterate through all the frames, and calculate all their scores."
     (should= 0 (score (new-game)))
     (should= 3 (score (Game. [(Frame. 1) (Frame. 2)])))
     )
+
+  (it "rolls all zeroes"
+    (let [game (reduce (fn [cum _] (roll cum 0) (new-game)) (range 20))]
+      (should= 0 (score game))
+      )
+    )
+
+  (it "rolls all ones"
+    (let [game (reduce (fn [cum _] (roll cum 1) (new-game)) (range 20))]
+      (should= 0 (score game))
+      )
+    )
+
   )
 
 (run-specs)
--- src/euler/level1/bowling.clj
+++ src/euler/level1/bowling.clj
@@ -5,5 +5,9 @@
 (defrecord Game [frames])
 (defn new-game [] (Game. []))
 
-(defn frame-score [frame] (:score frame))
+(defn frame-score [frame] (:score frame)) ; little sad that there can't be typed based function overloading https://github.com/ertugrulcetin/overload-fn
 (defn score [game] (reduce + (map frame-score (:frames game))))
+
+(defn roll [game pins]
+  (conj (:frames game) (Frame. pins))
+  )
